f16a734927ded531e9c0953dee4f3b2638459f72
David Bengoa <david@bengoa.me>
2018-02-03 17:57:03 +0100

    drop base-x dependency

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const blake2b = require('blakejs/blake2b');
-const base32 = require('base-x')('13456789abcdefghijkmnopqrstuwxyz');
 const nacl = require('tweetnacl-blake2b');
+
+const BASE_32 = '13456789abcdefghijkmnopqrstuwxyz'
 
 function toHex (uint8arr) {
   return Array.from(uint8arr).map(function(x) {
     return ('0' + x.toString(16)).substr(-2)
   }).join('');
+}
+
+function base32Encode(arr) {
+  if ((arr.length % 5) !== 0) throw new Error('can only encode strings where len(s)%5==0');
+  const chunks = [];
+
+  for(var o = 0, l = arr.length; o < l; o += 5) {
+    chunks.push(
+      BASE_32[  arr[o  ] >> 3                           ] +
+      BASE_32[((arr[o+1] >> 6) | (arr[o  ] << 2)) & 0x1F] +
+      BASE_32[ (arr[o+1] >> 1)                    & 0x1F] +
+      BASE_32[((arr[o+2] >> 4) | (arr[o+1] << 4)) & 0x1F] +
+      BASE_32[ (arr[o+3] >> 7) | (arr[o+2] << 1)  & 0x1F] +
+      BASE_32[ (arr[o+3] >> 2)                    & 0x1F] +
+      BASE_32[ (arr[o+4] >> 5) | (arr[o+3] << 3)  & 0x1F] +
+      BASE_32[  arr[o+4] & 0x1F                         ]
+    )
+  }
+
+  return chunks.join('');
 }
 
 function encodeAccountNumber (index) {
@@ -33,18 +54,23 @@ function publicKeyToAddress (publicKey) {
   padded.set(new Uint8Array(publicKey), 3);
   return (
     'xrb_' +
-    base32.encode(padded).substr(-52) +
-    base32.encode(blake2b.blake2b(publicKey, null, 5).reverse())
+    base32Encode(padded).substr(-52) +
+    base32Encode(blake2b.blake2b(publicKey, null, 5).reverse())
   );
 }
 
 function generateAddress (seed, accountNumber) {
   const secret = seedToSecretKey(seed, accountNumber);
+  return generateAddressFromSecretKey(secret);
+};
+
+function generateAddressFromSecretKey(secret) {
   const address = publicKeyToAddress(secretKeyToKeyPair(secret).publicKey);
 
   return { secret: toHex(secret), address: address };
-};
+}
 
 module.exports = {
-  generateAddress: generateAddress
+  generateAddress: generateAddress,
+  generateAddressFromPrivateKey: generateAddressFromSecretKey
 };
